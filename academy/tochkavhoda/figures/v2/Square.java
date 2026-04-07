212189ee80aef18cfddbdc89903b8644be2e0f5e
Viandr <testn12@inbox.ru>
2026-04-07 15:04:24 +0600

    module 10

---
+++ academy/tochkavhoda/figures/v2/Square.java
@@ -0,0 +1,10 @@
+package academy.tochkavhoda.figures.v2;
+
+import academy.tochkavhoda.Point;
+import academy.tochkavhoda.iface.v2.*;
+
+public class Square extends Figure implements Movable, Resizable, HasArea {
+    // ...existing code...
+    // (скопируйте реализацию из v1, поменяйте package; сохраняйте поля private)
+}
+
